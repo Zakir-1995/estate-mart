e1f796e245dad9ac372e08e7fc8e4555180a5fb1
Md Zakir Hossain <58882834+Zakir-1995@users.noreply.github.com>
2024-10-11 11:21:31 +0600

    connect DB

--- api/index.js
+++ api/index.js
@@ -1,10 +1,19 @@
-import express from "express"
-import dotenv from 'dotenv'
-dotenv.config()
-const app = express()
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+dotenv.config();
+const app = express();
 
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Mongodb Connected");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`)
-})
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
