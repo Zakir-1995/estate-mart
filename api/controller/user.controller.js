df21c44737e70260231b7659b7c6f416827a3f33
Md Zakir Hossain <58882834+Zakir-1995@users.noreply.github.com>
2024-10-14 08:36:28 +0600

    create User Update API

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -1,12 +1,51 @@
-
-
-
+import bcrypt from "bcryptjs";
+import User from "../models/user.model.js";
 export const getUsers = async (req, res) => {
   try {
     return res.status(200).json({
-      message:"works fine!",
+      message: "works fine!",
       error: false,
       success: true,
+    });
+  } catch (err) {
+    return res.status(200).json({
+      message: err.message || err,
+      error: true,
+      success: false,
+    });
+  }
+};
+
+export const updateUser = async (req, res) => {
+  const userId = req?.userId;
+  if (userId !== req?.params.id) {
+    return res.status(500).json({ message: "UnAuthorized!" });
+  }
+
+  try {
+    let hashedPassword;
+    if (req.body.password) {
+      hashedPassword = await bcrypt.hash(req.body.password, 10);
+    }
+
+    const updateUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: hashedPassword,
+          avatar: req.body.avatar,
+        },
+      },
+      { new: true }
+    );
+    const { password: pass, ...rest } = updateUser._doc;
+    return res.status(200).json({
+      message: "User Updated Successfully!",
+      error: false,
+      success: true,
+      data: rest,
     });
   } catch (err) {
     return res.status(200).json({
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,10 @@
 import express from 'express'
-import { getUsers } from "../controller/user.controller.js";
+import { getUsers, updateUser } from "../controller/user.controller.js";
+import {verifyToken} from '../utils/verifyToken.js'
 
 const router = express.Router()
 
 router.get("/users", getUsers);
+router.post("/update-user/:id",verifyToken, updateUser);
 
 export default router
