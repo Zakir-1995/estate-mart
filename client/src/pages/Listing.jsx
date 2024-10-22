0b8424d5ee15f39ba667fa26ec571de5cb2ea594
Md Zakir Hossain <58882834+Zakir-1995@users.noreply.github.com>
2024-10-22 09:26:53 +0600

    complete listing page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -6,6 +6,9 @@ import "swiper/css";
 import "swiper/css/navigation";
 import { Autoplay } from "swiper/modules";
 import { CgChevronLeftO, CgChevronRightO } from "react-icons/cg";
+import { MdLocationOn } from "react-icons/md";
+import { IoBed } from "react-icons/io5";
+import { FaBath, FaParking, FaChair } from "react-icons/fa";
 
 const Listing = () => {
   const [listingData, setListingData] = useState(null);
@@ -50,43 +53,117 @@ console.log(listingData);
       )}
 
       {listingData && !loading && !error && (
-        <div className="relative">
-          <Swiper
-            modules={[Autoplay]}
-            autoplay={{
-              delay: 5000,
-              disableOnInteraction: false,
-            }}
-            loop={true}
-            speed={2000}
-            className="mySwiper"
-            onBeforeInit={(swiper) => {
-              swiperRef.current = swiper;
-            }}
-          >
-            {listingData.images.map((image, index) => (
-              <SwiperSlide key={index} className="h-[500px] ">
-                <div
-                  className="h-full "
-                  style={{
-                    background: `url(${image?.url}) no-repeat center `,
-                  }}
-                ></div>
-              </SwiperSlide>
-            ))}
-          </Swiper>
-          <button
-            className="absolute top-[50%] z-50 -translate-y-[50%] left-10 opacity-70 hover:opacity-100 transition ease-linear duration-150"
-            onClick={() => swiperRef.current?.slidePrev()}
-          >
-            <CgChevronLeftO size={25} />
-          </button>
-          <button
-            className="absolute top-[50%] z-50 -translate-y-[50%] right-10 opacity-70 hover:opacity-100 transition ease-linear duration-150"
-            onClick={() => swiperRef.current?.slideNext()}
-          >
-            <CgChevronRightO size={25} />
-          </button>
+        <div>
+          <div className="relative">
+            <Swiper
+              modules={[Autoplay]}
+              autoplay={{
+                delay: 5000,
+                disableOnInteraction: false,
+              }}
+              loop={true}
+              speed={2000}
+              className="mySwiper"
+              onBeforeInit={(swiper) => {
+                swiperRef.current = swiper;
+              }}
+            >
+              {listingData.images.map((image, index) => (
+                <SwiperSlide key={index} className="h-[500px] ">
+                  <div
+                    className="h-full w-full"
+                    style={{
+                      background: `url(${image?.url}) no-repeat center `,
+                      objectFit: "cover",
+                    }}
+                  ></div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+            <button
+              className="absolute top-[50%] z-50 -translate-y-[50%] left-10 opacity-70 hover:opacity-100 transition ease-linear duration-150"
+              onClick={() => swiperRef.current?.slidePrev()}
+            >
+              <CgChevronLeftO size={25} />
+            </button>
+            <button
+              className="absolute top-[50%] z-50 -translate-y-[50%] right-10 opacity-70 hover:opacity-100 transition ease-linear duration-150"
+              onClick={() => swiperRef.current?.slideNext()}
+            >
+              <CgChevronRightO size={25} />
+            </button>
+          </div>
+          <div className="max-w-5xl mx-auto py-5 px-3 sm:px-5">
+            <h3 className="md:text-3xl lg:text-4xl text-2xl font-semibold text-gray-900">
+              {listingData?.name} - $
+              {listingData?.offer
+                ? listingData?.discountPrice
+                : listingData?.regularPrice}
+            </h3>
+            <div className="flex items-center gap-1 mt-5 ">
+              <span className="text-green-700 text-lg ">
+                <MdLocationOn />
+              </span>
+              <span className="text-sm font-medium text-gray-900">
+                {listingData?.address}
+              </span>
+            </div>
+            <div className="flex items-center gap-5 mt-5">
+              <span className="sm:text-xl text-lg font-medium bg-megenta w-[100px] flex justify-center text-white  rounded py-[2px]">
+                {listingData?.type === "rent" ? " For Rent" : "For Sell"}
+              </span>
+              {listingData?.offer && (
+                <span className="text-xl font-medium bg-green-700 w-[100px] flex justify-center text-white  rounded py-[2px]">
+                  $
+                  {`${
+                    listingData?.discountPrice
+                      ? listingData?.regularPrice - listingData?.discountPrice
+                      : null
+                  } `}{" "}
+                  Discount
+                </span>
+              )}
+            </div>
+            <p className="mt-5 text-sm font-medium text-gray-700">
+              {listingData?.description}
+            </p>
+            <div className="flex items-center gap-2 mt-3 flex-wrap">
+              <div className="flex items-center gap-1 text-sm font-medium text-green-700">
+                <span className="">
+                  <IoBed />
+                </span>
+                <span>
+                  {listingData?.bedroom}{" "}
+                  {listingData?.bedroom > 1 ? "Beds" : "Bed"}
+                </span>
+              </div>
+              <div className="flex items-center gap-2 text-sm font-medium text-green-700">
+                <span className="">
+                  <FaBath />
+                </span>
+                <span>
+                  {listingData?.bathroom}{" "}
+                  {listingData?.bathroom > 1 ? "Baths" : "Bath"}
+                </span>
+              </div>
+              <div className="flex items-center gap-2 text-sm font-medium text-green-700">
+                <span className="">
+                  <FaParking />
+                </span>
+                <span>
+                  {listingData?.parking ? "Parking Available" : "No Parking"}
+                </span>
+              </div>
+              <div className="flex items-center gap-2 text-sm font-medium text-green-700">
+                <span className="">
+                  <FaChair />
+                </span>
+                <span>
+                  {listingData?.furnished ? "Furnished" : "Not Furnished"}
+                </span>
+              </div>
+            </div>
+          </div>
         </div>
       )}
     </div>
